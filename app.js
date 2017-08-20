c3868277868c62358c7bf664a3ad417581ce3a97
Reji Mathews <reji@Rejis-MacBook-Pro.local>
2017-08-20 23:07:13 +0530

    single book retrieval implemented. book POST not working yet

--- app.js
+++ app.js
@@ -1,25 +1,70 @@
 var express = require("express");
+var mongoose = require("mongoose");
 var app = express();
 var port = process.env.PORT || 3000;
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+var bodyParser = require('body-parser');
+var bookRouter=express.Router(); 
+var Book = require('./models/bookModel'); // This is a mongoose schema. Can call  Book.find() Book.findById() Book.findByIdAndRemove() Book.findOne() Book.findOneAndUpdate()
 
-var bookRouter=express.Router();
+//Associating various modules with this app
 app.use('/api',bookRouter);
-bookRouter.route('/books')
+app.use(bodyParser.text); // Body parses is requred to parse the json object from http body and we do app.use to use it in this application
+app.use(bodyParser.urlencoded({extended:true}))
+
+//Router 1
+bookRouter.route('/Books')
+    //GET METHOD
     .get(function(req,res){
-        var response= {"message":"this is message from API"};
-        feedData(req);
-        res.json(response);
+        //var query = req.query; // This will parse the request query parameter from GET url. avoiding it as any such query will hit DB
+        // now we will enable query on certain query parameter only
+        var query = {}; // create empty query
+        if(req.query.genre){ //allow hook only for genre
+            query.genre =  req.query.genre;
+        }
+        Book.find(query,function(err,books){ // Passes query filters here 
+            if(err){
+                res.status(500).send(err);
+                console.log(err);
+            }
+            else
+                res.json(books);
+        });
     })
+    //POST METHOD
     .post(function(req,res){
-        res.send("Received post data")
+        console.log("Request body "+req.body)
+        var book = new Book(req.body) // This will create a new instance of book in mongodb and also generates the _id
+        console.log("Book to add "+book);
+        //book.save(); // This will save the record in mongodb
+        console.log(book);
+        res.send(book).status(201);
     });
+
+
+// Router 2     
+bookRouter.route('/books/:bookId')
+    .get(function(req,res){
+        console.log("Book id "+req.params.bookId)
+        Book.findById(req.params.bookId,function(err,book){ // Passes query filters here 
+            console.log("Book retrieved "+book)
+            if(err)
+                res.status(500).send(err);
+        
+                res.json(book).status(200);
+        });
+    });
+
+
+
+
 
 app.listen(port,function(){
     console.log("Server is listening on the port "+port);
 });
 
-function feedData(data){
- console.log("called function"+data);
+function someProcessingLogic(data){
+ console.log("called processing logic block - "+data);
 }
 
 //Root API get call
