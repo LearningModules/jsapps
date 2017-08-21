bf199c638309dd3ab5b6523dbb5c2f6aa9fd6777
Reji Mathews <reji@Rejis-MacBook-Pro.local>
2017-08-21 22:25:28 +0530

    book POST working.

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,7 +1,8 @@
 var express = require('express');
-//var Book = require('../models/bookModel'); // Will get this injected from app.js
+
+
+
 var routes = function(Book,jsonParser){ // declaring as function for testability. Book is injected from app.js
-//Router 1
 var bookRouter=express.Router(); 
 bookRouter.route('/Books')
     .get(function(req,res){ //GET METHOD
@@ -21,10 +22,9 @@ bookRouter.route('/Books')
         });
     })
     .post(jsonParser,function(req,res){ //POST METHOD
-        console.log("Request body "+req.body)
-        var book = new Book(req.body) // This will create a new instance of book in mongodb and also generates the _id
+        var book = new Book(req.body); // This will create a new instance of book in mongodb and also generates the _id
         console.log("Book to add "+book);
-        //book.save(); // This will save the record in mongodb
+        book.save(); // This will save the record in mongodb
         console.log(book);
         res.send(book).status(201);
     })
